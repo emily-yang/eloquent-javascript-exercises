39cf44f9535ebe84b94eefc4171d06f89d170361
emily-yang <cigoua@gmail.com>
2019-07-22 16:10:04 -0700

    solutions for 5-1 and 5-2

--- chapter-5_HOF/5-1_Flattening.js
+++ chapter-5_HOF/5-1_Flattening.js
@@ -1,14 +1,11 @@
 /*
-Use the reduce method in combination with the concat method to “flatten” an array of arrays 
+Use the reduce method in combination with the concat method to “flatten” an array of arrays
 into a single array that has all the elements of the original arrays.
 */
 
 const arrays = [[1, 2, 3], [4, 5], [6]];
 
-// Solution
-console.log(arrays.reduce((accum, curr) => accum.concat(curr)));
-
-// Alternative
-console.log(arrays.flat());
+console.log(arrays.reduce((flattened, arr) => flattened.concat(arr)));
+// console.log(arrays.flat());
 
 // → [1, 2, 3, 4, 5, 6]
